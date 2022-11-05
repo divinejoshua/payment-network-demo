ade8af69db5eb6757ff0ff5f0943f6f8f8fa18fc
divinejoshua <divineuche399@gmail.com>
2022-11-05 20:03:42 +0000

    Added page structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import './App.css';
+import shoe from'./assets/shoe.jpg'
 
 function App() {
   return (
     <div className="App">
+
+      {/* Header  */}
       <header className="App-header">
-     
+      Fancy Shoes
+
+        <ul>
+          <li>Men</li>
+          <li>Women</li>
+          <li>Kids</li>
+          <li>Sales</li>
+        </ul>
       </header>
 
-      <main>
-        <section className="left-side">
+    {/* Main body  */}
+      <main className='grid grid-cols-3 gap-4'>
+
+        {/* Left side  */}
+        <section className="left-side col-span-2">
+          <img src={shoe} />
         </section>
 
+        {/* Right side  */}
         <section className="right-side">
+          <h1>Nike Air Zoom Pegasus 39 Shield</h1>
+          <p>Women's Weatherised Road Running Shoes</p>
+          £119.96
+
+          {/* Sizes  */}
+          <div className="grid grid-cols-4 gap-4">
+            <div>UK 2.5</div>
+            <div>UK 3</div>
+            <div>UK 3.5</div>
+            <div>UK 4</div>
+            <div>UK 4.5</div>
+            <div>UK 5</div>
+            <div>UK 5.5</div>
+            <div>UK 6</div>
+          </div>
+          
+
+          <button>Buy</button>
         </section>
+
+
+
+
 
       </main>
 
