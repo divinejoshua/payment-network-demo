624ff7011e6ca30d6ad8d0a98dfec4c3864e4052
divinejoshua <divineuche399@gmail.com>
2022-11-05 20:36:40 +0000

    Added hover css on images

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
 
       {/* Header  */}
-      <header className="App-header grid grid-cols-3 gap-4 pt-8 pb-5 container mx-auto mb-5 border-b border-gray-100">
+      <header className="App-header grid grid-cols-3 gap-8 pt-8 pb-5 container mx-auto mb-5 border-b border-gray-100">
       <h1 className='font-bold col-span-2'>Fancy Shoes</h1>
 
         <ul className='flex right-0'>
@@ -19,12 +19,12 @@ function App() {
       </header>
 
     {/* Main body  */}
-      <main className='md:grid md:grid-cols-3 md:gap-4 container mx-auto mt-8'>
+      <main className='md:grid md:grid-cols-3 md:gap-8 container mx-auto mt-8'>
 
         {/* Left side  */}
         <section className="left-side col-span-2 grid grid-cols-2 gap-4">
-          <img src={shoe1}/>
-          <img src={shoe2}/>
+          <img src={shoe1} className="hover:scale-105 transition ease-in-out delay-150 rounded"/>
+          <img src={shoe2} className="hover:scale-105 transition ease-in-out delay-150 rounded"/>
         </section>
 
         {/* Right side  */}
